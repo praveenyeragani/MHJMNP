1990ecf98b4d6917da1a9a4d7bed1322bca5db4d
pickncart <urskrishn@gmail.com>
2015-01-03 02:12:31 +0530

    add users view

--- MilkAndPaper/src/org/milkandpaper/controller/DataController.java
+++ MilkAndPaper/src/org/milkandpaper/controller/DataController.java
@@ -71,12 +71,12 @@ public class DataController {
 		return new ModelAndView("login");
 	}
 	
-	@RequestMapping(value="Admin/users")
+	@RequestMapping(value="admin/users")
 	public ModelAndView getUsersList() {
 		List<Users> userList=dataService.getUserList();
 		ModelAndView modelView=new ModelAndView();
 		modelView.addObject("userList",userList);
-		modelView.setViewName("Admin/users");
+		modelView.setViewName("admin/users");
 		return modelView;
 	}
 	
